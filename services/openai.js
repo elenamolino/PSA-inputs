3f6dea8a821995baa1b1765c6acd7cdf53ae5504
Juan Carlos Cortés Muñoz <juacormun@alum.us.es>
2023-05-28 17:31:45 +0200

    Fix post request prompt

--- services/openai.js
+++ services/openai.js
@@ -32,8 +32,9 @@ const getRequestParams = async (request, json, numInputs) => {
 
 const postRequestParams = async (request, json, numInputs) => {
     const prompt = 'Hi, I am going to attach an API specification in JSON format. Could you return me a list including at least '
-    + numInputs + ' different real and valid examples for the query params and body params needed to make a POST request to ' + request + '? Please, I want '
-    + 'your response to follow this format: [{"query": {params1A}, "body": {params1B}}, {"query": {params2A}, "body": {params2B}}], it must be a valid JSON. Also, keep in mind that some dates should be in the future and today is ' + new Date() + '. \n\n ' + json;
+    + numInputs + ' different real and valid examples for the needed parameters to make a POST request to ' + request + '? Please, I want '
+    + 'your response to follow this format: [ { "headers": {params1A}, query": {params1B}, "body": {params1C} }, { "headers": {params2A}, "query": {params2B}, "body": {params2C} } ],'
+    + ' it must be a valid JSON. Also, keep in mind that some dates should be in the future and today is ' + new Date() + '. \n\n ' + json;
 
         try {
         const completion = await openai.createChatCompletion(
